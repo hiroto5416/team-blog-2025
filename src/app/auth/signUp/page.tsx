35de91ab8ed536074afa0a8fd5cbcb54ade25668
TakeoY <takeoycoding@gmail.com>
2025-03-13 11:26:05 +0900

    ターミナルで大文字フォルダ削除

--- src/app/auth/signUp/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignUpForm from "@/components/modules/auth/SignUpForm";
-
-export default function SignUpPage() {
-  return (
-    <div className="flex min-h-screen justify-center items-center">
-      <SignUpForm />
-    </div>
-  );
-}
