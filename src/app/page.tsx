c2304a422b4bf4cc33fb2c0fa65432906e8f6d27
IDA RIE <hrmi9417@gmail.com>
2025-03-14 00:12:26 +0900

    サインイン・サインアップページ作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import { Arrow } from '@/components/ui/custom/Arrow';
-import { AuthInput } from '@/components/ui/custom/AuthInput';
-import { SearchInput } from '@/components/ui/custom/SearchInput';
+import SignInForm from './auth/SignIn/page';
 // import { Spinner } from '@/components/ui/custom/Spinner';
 
 export default function Home() {
   return (
-    <div className="my-3 flex flex-col items-center justify-center gap-3">
-      <AuthInput label="name" name="Name" type="text" placeholder="Enter your name" />
-      <SearchInput />
+    <>
       {/* <Spinner /> */}
-      <Arrow />
-    </div>
+      <SignInForm />
+    </>
   );
 }
--- src/components/ui/custom/AuthInput.tsx
+++ src/components/ui/custom/AuthInput.tsx
@@ -6,14 +6,24 @@ type AuthInputProps = {
   type: string;
   name: string;
   placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  required?: boolean;
 };
 
 export function AuthInput(props: AuthInputProps) {
-  const { label, type, name, placeholder } = props;
+  const { label, type, name, placeholder, value, onChange, required } = props;
   return (
     <div className="grid w-full max-w-sm items-center gap-1.5">
       <Label htmlFor={label}>{name}</Label>
-      <Input type={type} id={label} placeholder={placeholder} />
+      <Input
+        type={type}
+        id={label}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        required={required}
+      />
     </div>
   );
 }
