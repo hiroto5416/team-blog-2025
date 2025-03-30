675e4584620557d5a0c82c8a6a0dd06876d154ad
kujira-challenge <kujira.challenge@gmail.com>
2025-03-30 22:43:08 +0900

    記事作成編集を中心とした修正 (#70)
    * 記事作成編集を中心とした修正
    
    * pakage-lock.jsonの内容をmainに合わせてプッシュ
    
    ---------
    
    Co-authored-by: kujira-challenge <kujira-challenge@gmail.com>

--- src/components/ui/custom/CreateButton.tsx
+++ src/components/ui/custom/CreateButton.tsx
@@ -1,14 +1,20 @@
+//src/components/ui/custom/CreateButton.tsx
+
 import React from "react";
 import { Button } from "@/components/ui/button";
 
-/**
- * クリック可能なボタンを作成するコンポーネント
- * @param {React.ReactNode} children - ボタンのテキスト
- * @param {() => void} onClick - クリック時のイベントハンドラ
- */
-export function CreateButton({ children, onClick }: { children: React.ReactNode; onClick?: () => void }) {
+export function CreateButton({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick?: () => void;
+}) {
   return (
-    <Button className="rounded-md px-4 py-2" onClick={onClick}>
+    <Button
+      onClick={onClick}
+      className="border border-[var(--color-accent-cyan)] text-[var(--color-accent-cyan)] hover:bg-[var(--color-accent-cyan)] hover:text-[var(--color-background)] px-6 py-2 rounded-full transition-colors duration-200"
+    >
       {children}
     </Button>
   );
