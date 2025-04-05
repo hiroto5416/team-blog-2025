4a7d191b6996bd5737118a074b65771479a016c7
TakeoY <takeoycoding@gmail.com>
2025-04-05 22:03:15 +0900

    記事投稿ページのAPIつなぎ込み

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    domains: ['dcdpsoyzvkvbihyothbr.supabase.co'], // ← あなたの Supabase プロジェクトの画像ホスト
+  },
+}
 
-export default nextConfig;
+export default nextConfig
